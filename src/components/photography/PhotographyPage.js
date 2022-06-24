e40e55f4e776c50624c96d2f583bf1abe6485c53
luism007 <luisgm.w001@gmail.com>
2022-06-24 08:46:43 -0700

    Re-added dropdown code

--- src/components/photography/PhotographyPage.js
+++ src/components/photography/PhotographyPage.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import DropdownComponent from "../common/dropdown/Dropdown";
 import List from "../common/list/List";
 import './PhotographyPage.css';
 
@@ -14,35 +15,59 @@ const PhotographyPage = () => {
         {
             source: "public/images/heart-of-the-canyon.jpg",
             title: "Nature",
-            location: "Page, AZ" 
+            location: "Page, AZ",
+            type: "nature"
         },
         {
             source: "public/images/detroit-fox-theatre-1.jpg",
             title: "Urban",
-            location: "Phoenix, AZ" 
+            location: "Phoenix, AZ",
+            type: "urban" 
         },
         {
             source: "public/images/doomslayer.jpg",
             title: "Portraits",
-            location: "Detroit, MI" 
+            location: "Detroit, MI",
+            type: "portrait"
         },
         {
             source: "public/images/detroit-night-street-inlimbo.jpg",
             title: "Wandering the Streets",
-            location: "Detroit, MI" 
+            location: "Detroit, MI",
+            type: "urban" 
         }
-    ]
+    ];
+
+    const types = ['nature', 'portrait', 'urban'];
 
     const retrieveList = () => {
         setTimeout(() => {
             setLoading(false);
         }, 2000);
     }
-    return(
-        <div className="photography-container">
-            { (loading) ? <p>Loading ...</p> : <List items = {pics}/> }
-        </div>
-    )
+
+    const captureSelectedOption = (option) => {
+        console.log(option);
+    }
+
+    return (
+      <div className="photography-container">
+        {loading ? (
+          <p>Loading ...</p>
+        ) : (
+          <div className="items-container">
+            <div className="dropdown-container">
+              <DropdownComponent
+                options={types}
+                label="Select Photos"
+                callback = { captureSelectedOption }
+              ></DropdownComponent>
+            </div>
+            <List items={pics} />
+          </div>
+        )}
+      </div>
+    );
 };
 
 export default PhotographyPage; 
