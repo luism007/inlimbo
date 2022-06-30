01e06cf8348d3168b1acc31e1521a77df69ecb24
luism007 <luisgm.w001@gmail.com>
2022-06-29 20:08:12 -0700

    Modified multiple components to administer new Spinner component and corresponding functionality

--- src/components/common/list/List.js
+++ src/components/common/list/List.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "../loading/Spinner";
 import PhotoTiles from "../photo-tiles/PhotoTiles";
 import './List.css'
 
 const List = (props) => {
+
+  useEffect(() => {
+    if(props.loading) { 
+       const spinner = document.getElementById('spin');
+       spinner.scrollIntoView({behavior: 'smooth', block: 'end'});
+    } else { 
+      const focusedId = `tile-${props.offset}`;
+      const tile = document.getElementById(focusedId);
+      (tile) ? tile.scrollIntoView({behavior: 'smooth', block: 'end'}) : null;
+    }
+  });
 
   const showMore = () => { 
     props.showMoreCallback();
@@ -10,17 +22,20 @@ const List = (props) => {
   
     return (
       <div className="list-container">
-        <ul className="list-item-container">
+        <ul className="list-item-container" id = "list">
           {" "}
           {props.items.map((item, index) => {
             return (
-              <li key={index} className="list-item">
+              <li key={index} id = {`tile-${index}`} className="list-item">
                 <PhotoTiles {...item}></PhotoTiles>
               </li>
             );
           })}{" "}
         </ul>
-        <button id = "showMoreButton" onClick={ showMore }> Show More </button>
+        { props.loading ? <Spinner/> : 
+        <span id = "showMoreButton" onClick={ showMore }>
+          <img src="public/images/dropdown-logo-light.png">
+            </img></span> }
       </div>
     );
 }
