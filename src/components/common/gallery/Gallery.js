d83b9364eceea33b4173a468939f3de99e95edce
luism007 <luisgm.w001@gmail.com>
2022-09-07 22:04:47 -0700

    Stylistic changes for gallery component.

--- src/components/common/gallery/Gallery.js
+++ src/components/common/gallery/Gallery.js
@@ -66,44 +66,50 @@ const Gallery = (props) => {
     }
 
     return (
-        <div className="overlay-container">
-          <div className="overlay-preview-container">
-            <div className="overlay-pic-showcase-container">
-              <div className="gallery-hide-button-container">
-                <button onClick={props.closeOverlay}>Hide</button>
-              </div>
-                <div className="overlay-gallery-button-left">
-                    <button onClick={prevPhoto}> prev </button>
-                </div>
-                <div className="overlay-pic-showcase">
-                    <img className="overlay-pic" id = "active-photo" src = {photoInView.source}></img>
-                </div>
-                <div className="overlay-gallery-button-right">
-                    <button onClick={nextPhoto}> next </button>
-                </div>
+      <div className="overlay-container">
+        <div className="overlay-preview-container">
+          <div className="overlay-pic-showcase-container">
+            <div className="gallery-hide-button-container">
+              <span className="close-btn"><img src="public/images/close-x.png" onClick={props.closeOverlay}></img></span>
             </div>
-            <div className="overlay-pic-grid-container">
-              <ul className="overlay-pics-container">
-                {props.photos.map((photo, index) => {
-                  return (
-                    <li
-                      key={index}
-                      id={`grid-photo-${index}`}
-                      className="grid-photo-list-item"
-                      onClick={()=>{ setPhoto(index)}}
-                    >
-                      <img src={photo.source} className = "grid-photo"></img>
-                    </li>
-                  );
-                })}
-              </ul>
+            <div className="overlay-gallery-button-left">
+              <span><img src = "public/images/left-arrow.png" onClick={prevPhoto}></img></span>
+            </div>
+            <div className="overlay-pic-showcase">
+              <img
+                className="overlay-pic"
+                id="active-photo"
+                src={photoInView.source}
+              ></img>
+            </div>
+            <div className="overlay-gallery-button-right">
+            <span><img src = "public/images/right-arrow.png" onClick={nextPhoto}></img></span>
             </div>
           </div>
-          <div className="overlay-pic-description-container">
-            <p className="overlay-pic-description"></p>
+          <div className="overlay-pic-grid-container">
+            <ul className="overlay-pics-container">
+              {props.photos.map((photo, index) => {
+                return (
+                  <li
+                    key={index}
+                    id={`grid-photo-${index}`}
+                    className="grid-photo-list-item"
+                    onClick={() => {
+                      setPhoto(index);
+                    }}
+                  >
+                    <img src={photo.source} className="grid-photo"></img>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
-    )
+        <div className="overlay-pic-description-container">
+          <p className="overlay-pic-description"></p>
+        </div>
+      </div>
+    );
 }
 
 export default Gallery;
