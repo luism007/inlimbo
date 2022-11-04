d42cfb7f628d5d4c45c649cc6f84b98465b16304
luism007 <luisgm.w001@gmail.com>
2022-11-03 19:30:30 -0700

    Added new animations. Creating photo-tile animation functionality

--- src/components/common/photo-tiles/PhotoTiles.js
+++ src/components/common/photo-tiles/PhotoTiles.js
@@ -1,10 +1,19 @@
 import React, { useEffect } from "react";
 import "./PhotoTiles.css";
 import PictureCommunicationService from '../../../rxjs-services/picture-service.js'
+import { 
+  fadeInKeyframes, 
+  fadeInOptions,
+  slideUpKeyframes,
+  slideUpKeyFrameOptions
+} from "../../../models/AnimationsModel";
 const PhotoTiles = (props) => {
+  useEffect(()=>{
+    animateTile();
+  }, [props])
   const animateTile = () => {
-    const tile = document.getElementById("tileBlock");
-    tile.classList.add("enter");
+   const tile = document.getElementById(props.id);
+   tile?.animate(fadeInKeyframes, fadeInOptions);
   }
   const focusOnPicture = () => {
     PictureCommunicationService.updatePictureSubject(props);
@@ -12,7 +21,7 @@ const PhotoTiles = (props) => {
     return (
      <div className="tile-container">
        <div className="tile" id = "tileBlock">
-         <img src = {props.source} loading = "lazy" height= "300px" width = "500px" onClick={focusOnPicture}></img>
+         <img id = {props.id} src = {props.source} loading = "lazy" height= "300px" width = "500px" onClick={focusOnPicture} className = "imageTile"></img>
        </div>
      </div>
     );
