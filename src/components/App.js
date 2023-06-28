b61dbd9d859e79bc896b0182d81d994537256424
luism007 <luisgm.w001@gmail.com>
2023-06-28 09:42:09 -0700

    Added final route-transition animations for pages in the site.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import PhotographyPage from "./photography/PhotographyPage";
 import NavHeader from "./common/NavHeader";
@@ -8,26 +8,33 @@ import "./App.css";
 import "../web-responsive.css";
 import inlimboLogo from '../../public/images/inlimbo-proto.svg';
 import AboutMe from "./about-me/AboutMe";
+import { AnimatePresence, motion } from "framer-motion";
 const App = () => {
-  
+  const location  = useLocation();
   return (
-  <div>
-    <div className="header">
-      <div className="logo">
-        <img src= "../../public/images/inlimbo-proto-blk-small.svg" alt = "inlimbo-logo" aria-label="INLIMBO"></img>
+    <div>
+      <div className="header">
+        <div className="logo">
+          <img
+            src="../../public/images/inlimbo-proto-blk-small.svg"
+            alt="inlimbo-logo"
+            aria-label="INLIMBO"
+          ></img>
+        </div>
       </div>
+      <NavHeader />
+      <AnimatePresence mode="wait">
+        <div className="page-showcase">
+            <Routes location={location} key={location.key} >
+              <Route path="/" element={<HomePage />} />
+              <Route path="/photography" element={<PhotographyPage />} />
+              <Route path="/aboutme" element={<AboutMe />} />
+              <Route element={<PageNotFound />} />
+            </Routes>
+        </div>
+      </AnimatePresence>
     </div>
-    <NavHeader />
-    <div className="page-showcase">
-      <Routes>
-        <Route path="/" element={ <HomePage/> } />
-        <Route path="/photography" element={ <PhotographyPage/> } />
-        <Route path="/aboutme" element={ <AboutMe/> } />
-        <Route element={ <PageNotFound/> } />
-      </Routes>
-    </div>
-  </div>
-)
+  );
 };
 
 export default App;
--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -7,6 +7,7 @@ import codeLogo from '../../../public/images/code.png';
 import userLogo from '../../../public/images/user.png'
 import "./NavHeader.css";
 import "../../web-responsive.css";
+import { motion } from "framer-motion";
 const NavHeader = () => {
     const activestyle = { 
         color: 'black',
@@ -16,16 +17,36 @@ const NavHeader = () => {
     return (
       <div className="inlimbo-navbar">
         <nav>
-          <NavLink to="/" style={activestyle} exact = "true">
-            Home
-          </NavLink>{" "}
-          <NavLink to="/photography" style={activestyle}>
+          <motion.span
+            initial = {{opacity: 0, y: '50%'}}
+            animate = {{opacity: 1, y: '0' }}
+            exit = {{opacity: 0 , y: '0'}}
+            transition={{duration: 1, repeat: 0}}
+          >
+            <NavLink to="/"exact = "true" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }>
+              Home
+            </NavLink>
+          </motion.span>
+          <motion.span
+            initial = {{opacity: 0, y: '50%'}}
+            animate = {{opacity: 1, y: '0' }}
+            exit = {{opacity: 1 , y: '0'}}
+          transition={{duration: 1, repeat: 0}}
+          >
+          <NavLink to="/photography" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }>
             Gallery
-          </NavLink>{" "}
-          <NavLink to="/courses" style={activestyle}>
-            {" "}
+          </NavLink>
+          </motion.span>
+          <motion.span
+            initial = {{opacity: 0, y: '50%'}}
+            animate = {{opacity: 1, y: '0' }}
+            exit = {{opacity: 1 , y: '0'}}
+          transition={{duration: 1, repeat: 0}}
+          >
+          <NavLink to="/aboutme" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }>
             About Me
-          </NavLink>{" "}
+          </NavLink>
+          </motion.span>
         </nav>
       </div>
     );
