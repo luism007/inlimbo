21bd45f9d3dd9259f393c03b505c428317660658
luism007 <luisgm.w001@gmail.com>
2023-07-06 08:18:11 -0700

    Enhancing section-scroller functionality & UX. Still need some additional touch ups

--- src/components/common/section-scroller/SectionScroller.js
+++ src/components/common/section-scroller/SectionScroller.js
@@ -1,26 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import './SectionScroller.css';
 const SectionScroller = (props) => {
 
+    const [ellipsisLocation, setEllipsisLocation] = useState('');
+
     useEffect(() => {
 
-    }, [props]);
+    }, [props, ellipsisLocation]);
 
-    const scrollToSection = (sectionLink) => {
-        console.log('Section Link', sectionLink);
+    const scrollToSection = (sectionLink, index) => {
         const section = document.getElementById(sectionLink);
-        console.log('Clicked!', section);
+        const sectionScrollLink = document.getElementById(`${sectionLink}-${index}`);
+        const ellipsis = document.getElementById('ellipsis');
+        const originalLocation = ellipsisLocation.match(/^/) ? ellipsisLocation : ellipsis.offsetTop;
+        const location = sectionScrollLink.offsetTop + (sectionScrollLink.offsetHeight / 2);
+        setEllipsisLocation(`${location}px`);
+        const keyframes = [
+            { transform: `translateY(${originalLocation}px)`, opacity: 0},
+            { transform:  `translateY(${location}px)`, opacity: 1}
+        ];
+        const timing = {
+            duration: 700,
+            fill: 'both'
+        };
+        ellipsis.animate(keyframes, timing);
         section.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'start'});
     }
     return (
         <div className="section-scroller-container">
             <div className="section-list-container">
                 <ul>
+                    <span id = "ellipsis"></span>
                     {props.sections.map((section, index) => {
                         return (
                           <div className="section-list-item-container" key={index}>
-                            <span></span>
-                            <li className="section-list-item" onClick={() => { scrollToSection(section) }}> {section} </li>
+                            <li className="section-list-item" onClick={() => { scrollToSection(section, index) }} id = {`${section}-${index}`}> {section} </li>
                             <span className="section-list-divider"></span>
                           </div>
                         );
