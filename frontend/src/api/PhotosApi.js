af9eb9a0dd6979485f2710d7717a2ec345092119
luism007 <luisgm.w001@gmail.com>
2023-07-17 15:02:18 -0700

    Still getting some issues. But managed to find a fix to a typo. Getting closer to getting this sorted

--- frontend/src/api/PhotosApi.js
+++ frontend/src/api/PhotosApi.js
@@ -1,16 +1,16 @@
 export const getPhotos = async () => {
-    const response = await fetch(`/api/photography`);
+    const response = await fetch(`${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}/api/photography`);
     return response.json();
 }
 
 export const getPhotosByOffset = async (offset, limit) => {
-    const response = await fetch(`/api/photography?offset=${offset}&limit=${limit}`);
+    const response = await fetch(`${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}/api/photography?offset=${offset}&limit=${limit}`);
     return response.json();
 }
 
 export const getPhotosByCollectionId = async (collectionId, originalPhotoId) => {
     const ob = {_id: originalPhotoId, collectionId: collectionId};
-    const url = `/api/collection`;
+    const url = `${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}/api/collection`;
     const response = await fetch(url, {
         method: 'POST',
         headers: {
