f601ccdd4f5de5048ea757fe677208dcbcb1bad4
luism007 <luisgm.w001@gmail.com>
2023-08-09 11:10:02 -0700

    More fixes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,6 +11,7 @@ import { AnimatePresence } from "framer-motion";
 import inlimboLogo from '../assets/inlimbo-light-gray-on-white.svg'
 import instagramIcon from '../assets/instagram-icon.svg';
 import mailIcon from '../assets/envelope-icon.svg';
+import Footer from "./common/footer/Footer";
 const App = () => {
   const location  = useLocation();
   return (
@@ -29,50 +30,7 @@ const App = () => {
             <Route path="/aboutme" element={<AboutMe />} />
             <Route element={<PageNotFound />} />
           </Routes>
-          <footer>
-            <div className="footer-container" id="footer">
-              <div id="footer-wrapper">
-                <div className="footer-logo-wrapper">
-                  <img
-                    src={inlimboLogo}
-                    alt="inlimbo-logo"
-                    aria-label="INLIMBO"
-                  ></img>
-                </div>
-                <ul className="footer-contact-list">
-                  <li>
-                    <div className="footer-contact-list-item-wrapper">
-                      <img src={mailIcon}></img>
-                      <a
-                        className="footer-list-items-text"
-                        href="mailto:inlimbo.photography@gmail.com?subject=Service%20Inquiry"
-                        target="_top"
-                      >
-                        {" "}
-                        Contact Me{" "}
-                      </a>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="footer-contact-list-item-wrapper">
-                      <img src={instagramIcon}></img>
-                      <a
-                        className="footer-list-items-text"
-                        href="https://www.instagram.com/inlimbo.studios/"
-                      >
-                        {" "}
-                        Follow Me{" "}
-                      </a>
-                    </div>
-                  </li>
-                </ul>
-                <p className="footer-list-items-text">
-                  {" "}
-                  Developed & Designed by INLIMBO 2023
-                </p>
-              </div>
-            </div>
-          </footer>
+          <Footer></Footer>
         </div>
       </AnimatePresence>
     </div>
