bb75f72e9d90be837426e60f08f902d21202e6a9
luism007 <luisgm.w001@gmail.com>
2024-02-06 13:15:20 -0800

    Adding prod URL to PhotosAPI REST calls

--- frontend/src/api/PhotosApi.js
+++ frontend/src/api/PhotosApi.js
@@ -1,17 +1,17 @@
 export const getPhotos = async () => {
-    const url = (process.env.NODE_ENV == 'production') ? null : process.env.REACT_APP_API_URL_DEV;
+    const url = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV;
     const response = await fetch(`${url}/api/photography`);
     return response.json();
 }
 
 export const getPhotosByOffset = async (offset, limit) => {
-    const url = (process.env.NODE_ENV == 'production') ? null : process.env.REACT_APP_API_URL_DEV;
+    const url = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV;
     const response = await fetch(`${url}/api/photography?offset=${offset}&limit=${limit}`);
     return response.json();
 }
 
 export const getPhotosByCollectionId = async (collectionId, originalPhotoId) => {
-    const apiUrl = (process.env.NODE_ENV == 'production') ? null : process.env.REACT_APP_API_URL_DEV;
+    const apiUrl = (process.env.NODE_ENV == 'production') ? process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV;
     const ob = {_id: originalPhotoId, collectionId: collectionId};
     const url = `${apiUrl}/api/collection`;
     const response = await fetch(url, {
